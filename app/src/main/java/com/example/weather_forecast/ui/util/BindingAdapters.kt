d0550591e1595c5142ac986ff4d4e313275a1491
helmy <mohamed0132525715@gmail.com>
2022-11-03 10:52:52 +0200

    Implement Home ui

---
+++ app/src/main/java/com/example/weather_forecast/ui/util/BindingAdapters.kt
@@ -0,0 +1,40 @@
+package com.example.weather_forecast.ui.util
+
+import android.annotation.SuppressLint
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.weather_forecast.domain.model.WeatherType
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.math.roundToInt
+
+@BindingAdapter("weatherType")
+fun ImageView.bindWeatherTypeImage(weatherType: WeatherType) {
+    setImageResource(weatherType.iconRes)
+    contentDescription = weatherType.weatherDesc
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("today")
+fun TextView.bindDayText(time: LocalDateTime) {
+    text = "Today, " + time.format(DateTimeFormatter.ofPattern("d MMMM"))
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("weatherTemperature")
+fun TextView.bindTemperatureText(temp: Double) {
+    text = "${temp.roundToInt()}°C"
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("weatherWindSpeed")
+fun TextView.bindWindSpeedText(speed: Double) {
+    text = "Wind  |  ${speed.roundToInt()} km/h"
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("weatherHumidity")
+fun TextView.bindHumidityText(hum: Double) {
+    text = "Hum  |  ${hum.roundToInt()} %"
+}

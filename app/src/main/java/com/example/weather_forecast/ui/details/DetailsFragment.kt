0f2934a9d72e76bcffb44e678e4412bb642eeb79
Eng.FOda_H@g@g <fadyhagag32@gmail.com>
2022-11-06 19:35:35 +0200

    added adapter TodayRecyclerAdapter.kt but i found an error in DetailsFragment.kt ( layoutmanager)

--- app/src/main/java/com/example/weather_forecast/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/weather_forecast/ui/details/DetailsFragment.kt
@@ -5,21 +5,90 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.weather_forecast.R
 import com.example.weather_forecast.databinding.FragmentDetailsBinding
+import com.example.weather_forecast.domain.model.WeatherDetails
+import com.example.weather_forecast.domain.model.WeatherType
+import java.time.LocalDateTime
 
 
 class DetailsFragment : Fragment() {
 
-   private lateinit var binding:FragmentDetailsBinding
+    private lateinit var binding: FragmentDetailsBinding
+    private lateinit  var manager:RecyclerView.LayoutManager
+
+    private val listToDay = listOf(
+        WeatherDetails(
+            LocalDateTime.now(),
+            25.0, 1012.2, 9.0, 89.0,
+            WeatherType.fromWMO(2)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(),
+            55.0, 1012.2, 9.0, 89.0,
+            WeatherType.fromWMO(45)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(),
+            25.0, 1012.2, 9.0, 89.0,
+            WeatherType.fromWMO(55)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(),
+            25.0, 1012.2, 9.0, 89.0,
+            WeatherType.fromWMO(77)
+        )
+    )
+    private val listForcast = listOf(
+        WeatherDetails(
+            LocalDateTime.now(), 25.0, 1015.2, 10.0, 90.0,
+            WeatherType.fromWMO(5)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(), 25.0, 1015.2, 10.0, 90.0,
+            WeatherType.fromWMO(5)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(), 25.0, 1015.2, 10.0, 90.0,
+            WeatherType.fromWMO(5)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(), 25.0, 1015.2, 10.0, 90.0,
+            WeatherType.fromWMO(5)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(), 25.0, 1015.2, 10.0, 90.0,
+            WeatherType.fromWMO(5)
+        ),
+        WeatherDetails(
+            LocalDateTime.now(), 25.0, 1015.2, 10.0, 90.0,
+            WeatherType.fromWMO(5)
+        ),
+    )
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentDetailsBinding.inflate(layoutInflater,container,false)
+        binding = FragmentDetailsBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val todayAdapter=TodayRecyclerAdapter(listToDay)
+        binding.recyclerViewToday.adapter=todayAdapter
+
+        manager=LinearLayoutManager(context)
+        binding.recyclerViewToday.apply {
+            adapter=todayAdapter
+            LayoutManager=manager
+        }
     }
 
 
--- app/src/main/java/com/example/weather_forecast/ui/details/TodayRecyclerAdapter.kt
+++ app/src/main/java/com/example/weather_forecast/ui/details/TodayRecyclerAdapter.kt
@@ -1,6 +1,33 @@
 package com.example.weather_forecast.ui.details
 
-class TodayRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weather_forecast.databinding.CustomeHourItemBinding
+import com.example.weather_forecast.domain.model.WeatherDetails
 
-    class TodayViewHolder()
+class TodayRecyclerAdapter(private val data:List<WeatherDetails>):RecyclerView.Adapter<TodayRecyclerAdapter.TodayViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayViewHolder {
+        val listOfDay = CustomeHourItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return TodayViewHolder(listOfDay)
+    }
+
+    override fun onBindViewHolder(holder: TodayViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class TodayViewHolder(val binding : CustomeHourItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item:WeatherDetails){
+            binding.itemHour=item
+        }
+
+    }
 }
